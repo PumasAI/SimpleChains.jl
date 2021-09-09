d78e8126aa50baad8356112b218515cdeb301eb6
Chris Elrod <elrodc@gmail.com>
2021-09-09 11:42:38 -0400

    Rerun tests after trying to set up documenter.

--- src/dropout.jl
+++ src/dropout.jl
@@ -1,5 +1,13 @@
 using VectorizedRNG
 
+"""
+  Dropout(p) # 0 < p < 1
+
+Dropout layer.
+
+When evaluated without gradients, it multiplies inputs by `(1 - p)`.
+When evaluated with gradients, it randomly zeros `p` proportion of inputs.
+"""
 struct Dropout{R <: Union{Nothing,VectorizedRNG.AbstractRNG}}
   p::UInt32
   rng::R
