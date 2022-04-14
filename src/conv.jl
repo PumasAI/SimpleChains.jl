cf3aa77b7a221b0ede17d1a4ef3d33dacb9fe75d
Chris Elrod <elrodc@gmail.com>
2022-04-14 09:13:54 -0400

    Update src/conv.jl
    Co-authored-by: Niklas Korsbo <niklas.korsbo@gmail.com>

--- src/conv.jl
+++ src/conv.jl
@@ -689,7 +689,7 @@ adds a bias (one per `outputdim`) and applies `activation` elementwise.
 E.g., `Conv(relu, (5, 5), 16)` performs a `5 × 5` convolution, and maps the input
 channels to 16 output channels, before adding a bias and applying `relu`.
 
-Randomly initializing weights using the (Xaviar) Glorot uniform distribution.
+Randomly initializing weights using the (Xavier) Glorot uniform distribution.
 """
 struct Conv{F,D<:Tuple{Vararg{Integer}},O<:Integer}
   dim::D
