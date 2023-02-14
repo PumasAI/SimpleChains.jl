bbd81fa2a6ab2218402bccf312b6b7945075083d
Chris Elrod <elrodc@gmail.com>
2023-02-14 18:54:14 -0500

    Use Threads.nthreads, not CPUSummary.num_threads

--- src/SimpleChains.jl
+++ src/SimpleChains.jl
@@ -30,7 +30,7 @@ using SIMDTypes: Bit, NativeTypes
 using VectorizationBase: align, relu, stridedpointer, AbstractSIMD, NativeTypesV
 using HostCPUFeatures:
   static_sizeof, register_size, register_count, static_sizeof
-using CPUSummary: cache_linesize, num_threads, num_cores
+using CPUSummary: cache_linesize, num_cores
 using LayoutPointers:
   bytestrideindex, stridedpointer, zstridedpointer, zero_offsets, val_dense_dims
 using Static: One, lt
