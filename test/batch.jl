c95e8bcdd26f0ff27b1399d153d5ac09c1621665
chriselrod <elrodc@gmail.com>
2023-08-28 18:31:10 -0400

    copy output, stricter cmp

--- test/batch.jl
+++ test/batch.jl
@@ -7,14 +7,14 @@ using SimpleChains, Test
     TurboDense{true}(identity, 4)
   )
   opt = SimpleChains.ADAM()
-  penalty = L2Penalty(0.1)
+  penalty = L2Penalty(0.01)
   loss = SquaredLoss
 
   p0 = SimpleChains.init_params(chain)
-  y = chain(x, p0)
+  y = Matrix(chain(x, p0))
   model = penalty(SimpleChains.add_loss(chain, loss(y)))
   p1 = SimpleChains.init_params(chain)
   origloss = model(x, p1)
   @time SimpleChains.train_batched!(copy(p1), p1, model, x, opt, 10_000)
-  @test model(x, p1) < origloss
+  @test model(x, p1) < 0.25origloss
 end
